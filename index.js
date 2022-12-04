f860a23db1b9689d165ba8f418d86d85d9b6ea02
Raihan <Raihan jishan@gmail.com>
2022-12-04 17:36:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+## Hi there, I'm [Samin Yasar][website] <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px">
+## A passionate self-taught programmer from Bangladesh.
+
+[![Website](https://img.shields.io/website?label=saminyasar%20🚀&name=hello&style=flat&url=https://saminyasar.netlify.app/)](https://saminyasar.netlify.app/)
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-Follow-brightgreen)](https://linkedin.com/in/saminyasar2004/)
+[![Twitter Follow](https://img.shields.io/badge/Twitter-Follow-brightgreen)](https://twitter.com/saminyasar004/)
+[![Facebook Follow](https://img.shields.io/badge/Facebook-Follow-brightgreen)](https://www.facebook.com/saminyasar004/)
+[![Facebook Page](https://img.shields.io/badge/Facebook-Page-brightgreen)](https://www.facebook.com/saminyasar04/)
+[![Instagram Follow](https://img.shields.io/badge/Instagram-Follow-brightgreen)](https://instagram.com/saminyasar004/)
+[![Stack Overflow](https://img.shields.io/badge/Stack%20Overflow-Questions-brightgreen)](https://stackoverflow.com/users/14735945/samin-yasar)
+[![Profile Views](https://komarev.com/ghpvc/?username=saminyasar004&label=Profile%20views&color=brightgreen&style=flat)](https://github.com/saminyasar004/)
+[![Github Follow](https://img.shields.io/github/followers/saminyasar004?label=saminyasar004&style=social)](https://github.com/saminyasar004/)
+[![wakatime](https://wakatime.com/badge/user/d588c021-fa55-48f4-bb5b-e7283674eea2.svg?style=flat)](https://wakatime.com/@d588c021-fa55-48f4-bb5b-e7283674eea2)
+
+
+## I'm a Student, Programming Enthusiast , Web Designer & Developer!
+I am a student of HSC in Khilgaon Government College in Bangladesh. I like to learn programming. I have skils on HTML, CSS, JavaScript, ReactJS, NodeJS, ExpressJS, PHP & C.
+
+- 🔭 I’m currently working on Self Taught Programmer.
+- 🌱 I’m currently learning everything about Full Stack Web Development.
+- 👯 I’m looking to collaborate on JavaScript Enthusiast.
+- 🤔 I’m looking for help with Programmer.
+- 💬 Ask me about anything
+- 😄 Pronouns: He/Him
+- ⚡ Fun fact: 👕 T-Shirt Lover.
+
+### Connect with me:
+
+[<img align="left" alt="saminyasar004 |  Portfolio" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" />][website]
+[<img align="left" alt="saminyasar004 | LinkedIn" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" />][linkedin]
+[<img align="left" alt="saminyasar004 | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" />][twitter]
+[<img align="left" alt="saminyasar004 | Facebook" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/facebook.svg" />][facebook]
+[<img align="left" alt="saminyasar004 | Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />][instagram]
+<a href="https://dev.to/saminyasar004">
+  <img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Samin Yasar's DEV Community Profile" height="30" width="30">
+</a>
+<br />
+
+### Languages and Tools:
+<p align="left">
+  <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5"     width="40" height="40"/></a>
+  <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"             alt="css3" width="40" height="40"/> </a>
+  <a href="https://sass-lang.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/></a>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img    src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/></a>
+  <a href="https://www.php.net" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/></a>
+  <a href="https://getbootstrap.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"           alt="bootstrap" width="40" height="40"/></a>
+  <a href="https://reactjs.org/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/></a>
+  <a href="https://nodejs.org/en/" target="_blank"><img alt="Node.js" width="40" height="40" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png" /></a>
+    <a href="https://babeljs.io/" target="_blank"> <img src="https://www.vectorlogo.zone/logos/babeljs/babeljs-icon.svg" alt="babel" width="40" height="40"/></a>
+  <a href="https://code.visualstudio.com/" target="_blank"><img alt="Visual Studio Code" width="35" height="35" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png" /></a>
+  <a href="https://www.mysql.com/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/></a>
+  <a href="https://postman.com" target="_blank"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/></a>  
+  <a href="https://www.photoshop.com/en" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/photoshop/photoshop-line.svg" alt="photoshop" width="40" height="40"/></a>
+</p>
+
+<p><img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=saminyasar004&layout=compact&theme=tokyonight" alt="saminyasar004" /></p>
+<br />
+<p><a href="https://saminyasar.netlify.app/" target="_blank"><img align="center" src="https://github-readme-stats-anuraghazra1.vercel.app/api/pin/?username=saminyasar004&repo=saminyasar&theme=tokyonight" alt="saminyasar004" /></a></p>
+<br />
+<p><img align="center" src="https://github-readme-stats.vercel.app/api?username=saminyasar004&show_icons=true&theme=tokyonight" alt="saminyasar004" /></p>
+<br />
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=saminyasar004&theme=tokyonight" alt="saminyasar004" /></p>
+<br />
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=saminyasar004&theme=tokyonight" alt="saminyasar004" /></a></p>
+<br />
+
+[website]: https://saminyasar.netlify.app/
+[linkedin]: https://linkedin.com/in/saminyasar2004/
+[twitter]: https://twitter.com/saminyasar004/
+[instagram]: https://www.instagram.com/saminyasar04/
+[facebook]: https://www.facebook.com/saminyasar004/
---
+++ readme.md
@@ -0,0 +1,9 @@
+# `Plan proejcts list in single static html file. `
+### This is a simple html css merjup file to find a biginner for  building projects.. 
+### `info` and this file was uploaded in github pages ... 
+## `Github pages live link` {`${this.livelink}`};
+
+## `For more update keep following github `
+### github profile link : https://github.com/raihan-jishan
+
+### loved languages : html, css , js , React. vue, nodejs, express js, php ... so on ...
